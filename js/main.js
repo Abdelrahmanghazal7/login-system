431da344d0d64b577966663bcc680e2d3086d49a
Abdelrahman Ghazal <116028780+Abdelrahmanghazal7@users.noreply.github.com>
2023-12-29 22:43:39 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -91,18 +91,90 @@ function Exist() {
     return false;
 }
 
-// F O R G E T   P A S S W O R D
+//  C H A N G E   P A S S W O R D 
 
 function changePassword() {
-    var existEmail = window.prompt('Please Enter Your Email');
-    var newPassword = window.prompt('Please Enter Your New Password');
+    var existPasswordEmail = document.getElementById('existPasswordEmail').value;
+    var newPassword = document.getElementById('newPassword').value;
 
-    var foundUser = signUpArray.find(user => user.email.toLowerCase() === existEmail.toLowerCase());
+    var foundUser = signUpArray.find(user => user.email.toLowerCase() === existPasswordEmail.toLowerCase());
     if (foundUser) {
         foundUser.password = newPassword;
         localStorage.setItem('users', JSON.stringify(signUpArray));
-        alert('Password changed successfully!');
+        showToast('Password changed successfully, ' + foundUser.name + '!');
+        document.getElementById('passwordError').innerHTML = '';
     } else {
-        alert('User not found .. Please sign up first or provide a valid email');
+        document.getElementById('passwordError').innerHTML = '<span class="error">User not found .. Please sign up first or provide a valid email</span>';
     }
+}
+
+// C H A N G E   N A M E
+
+function changeName() {
+    var existNameEmail = document.getElementById('existNameEmail').value;
+    var newName = document.getElementById('newName').value;
+
+    var foundUser = signUpArray.find(user => user.email.toLowerCase() === existNameEmail.toLowerCase());
+    if (foundUser) {
+        foundUser.name = newName;
+        localStorage.setItem('users', JSON.stringify(signUpArray));
+        showToast('Name changed successfully!');
+        document.getElementById('nameError').innerHTML = '';
+    } else {
+        document.getElementById('nameError').innerHTML = '<span class="error">User not found .. Please sign up first or provide a valid email</span>';
+    }
+}
+
+// D E L E T E  U S E R
+
+function deleteAccount() {
+    var existdeletEmail = document.getElementById('existdeletEmail').value;
+
+    var userToDelete = signUpArray.find(user => user.email.toLowerCase() === existdeletEmail.toLowerCase());
+
+    if (userToDelete) {
+        var index = signUpArray.indexOf(userToDelete);
+        signUpArray.splice(index, 1);
+        localStorage.setItem('users', JSON.stringify(signUpArray));
+        showToast('Account deleted successfully!');
+        document.getElementById('emailError').innerHTML = '';
+    } else {
+        document.getElementById('emailError').innerHTML = '<span class="error">User not found .. Please provide a valid email</span>';
+    }
+}
+
+// T O G G L E   M E N U
+
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById('toggle');
+    var header = document.querySelector('header');
+
+    // Toggle menu on button click
+    toggleButton.addEventListener('click', function () {
+        header.classList.toggle('open');
+    });
+
+    // Close menu when clicking outside
+    document.addEventListener('click', function (event) {
+        var isClickInsideMenu = header.contains(event.target);
+        var isClickOnToggleButton = toggleButton.contains(event.target);
+
+        if (!isClickInsideMenu && !isClickOnToggleButton) {
+            header.classList.remove('open');
+        }
+    });
+});
+
+// S U C C E S S   M E S S A G E
+
+function showToast(message) {
+    var toast = document.getElementById('toast');
+    var toastMessage = document.getElementById('toastMessage');
+
+    toastMessage.innerText = message;
+    toast.classList.add('active');
+
+    setTimeout(function () {
+        toast.classList.remove('active');
+    }, 3000);
 }
